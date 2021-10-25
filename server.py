59d44e0ac4a849850591589f0cd5198af626f6cc
Tranzorro <shiekadam@gmail.com>
2021-10-24 21:14:20 -0400

    using a database
    accessing with flask from localhost

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, redirect, request
+from friend import Friend
+app = Flask(__name__)
+@app.route('/')
+def default():
+    friends = Friend.get_all()
+    print(friends)
+    return render_template("index.html", all_friends = friends)
+
+@app.route('/create_friend', methods=["POST"])
+def create_friend():
+    data = {
+        "fname": request.form["fname"],
+        "lname": request.form["lname"],
+        "occ": request.form["occ"]
+    }
+    Friend.save(data)
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
